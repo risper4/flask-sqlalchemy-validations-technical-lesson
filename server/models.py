7dd6e830ddbc89b1788c2d5eb9300e280da175e9
Risper <risperwanjikugichia@gmail.com>
2026-07-24 16:46:35 +0300

    Solution

--- server/models.py
+++ server/models.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-db = SQLAlchemy()
+from sqlalchemy import MetaData
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
 
 class EmailAddress(db.Model):
     __tablename__ = 'emailaddress'
@@ -11,7 +15,27 @@ class EmailAddress(db.Model):
 
     @validates('email', 'backup_email')
     def validate_email(self, key, address):
+        if not address :
+            raise ValueError('Email must be present')
+
+        if not isinstance(address, str):
+            raise ValueError('Email should be a string')
+
+        if len(address) > 254 :
+            raise ValueError('Email is too long')
+
         if '@' not in address:
             raise ValueError("Email must have an '@' in the address")
 
+        if address.split('@')[1] in ['hotmail.com', 'yahoo.com'] :
+            raise ValueError('Email cannot be a hotmail or yahoo address')
+
+        
         return address
+    
+
+# duplicate_email = db.session.query(EmailAddress.id)
+# if duplicate_email is not None :
+#     raise ValueError('Email must be unique')
+
+
